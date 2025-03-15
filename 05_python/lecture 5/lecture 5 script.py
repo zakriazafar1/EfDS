458237a70323567449937471ea3501c26086fcb9
Your Name <zakria@live.nl>
2025-03-15 12:53:10 +0100

    x

--- 05_python/lecture 5/lecture 5 script.py
+++ 05_python/lecture 5/lecture 5 script.py
@@ -1,1 +1,10 @@
-print("Hello world")
+# every now & then we have to write a python script 
+# we need more parameters, we need the command line for this
+
+import sys 
+fileName = sys.argv()
+
+with open(fileName, 'r') as file:
+    lines = file.readlines()
+    
+print("The file has", len(lines), "lines.")
