7b66e652cefbbe7f1494b1c76c176fb69fe09041
Zakria Zafar <zakria@live.nl>
2025-06-07 14:46:12 +0200

    ?

---
+++ 05_python/lecture 5/gen_norm_nums.py
@@ -0,0 +1,18 @@
+import statistics 
+import sys 
+import random 
+
+if __name__ == "__main__":
+    fileName = sys.argv[1]
+    size = int(sys.argv[2])
+    mu = float(sys.argv[3])
+    sd = float(sys.argv[4])
+
+vs = [random.gauss(mu=mu, sigma=sd) for i in range(size)]
+    print( f"Size:   {size}" )
+    print( f"Mean:   requested={mu}, generated={statistics.mean(vs)}" )
+    print( f"Stddev: requested={sd}, generated={statistics.stdev(vs)}" )
+    
+with open(file=fileName, mode="w") as f:
+        for v in vs:
+            print( v, file=f )
