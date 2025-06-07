f6e57d08124a9d196ca44d5aae3e189f7c225278
Zakria Zafar <zakria@live.nl>
2025-06-07 14:51:32 +0200

    svript

--- 05_python/gen_norm_nums.py
+++ 05_python/gen_norm_nums.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     sd = float(sys.argv[4])
 
 vs = [random.gauss(mu=mu, sigma=sd) for i in range(size)]
-    print( f"Size:   {size}" )
-    print( f"Mean:   requested={mu}, generated={statistics.mean(vs)}" )
-    print( f"Stddev: requested={sd}, generated={statistics.stdev(vs)}" )
+
+print( f"Size: {size}")
+print( f"Mean: requested={mu}, generated={statistics.mean(vs)}" )
+print( f"Stddev: requested={sd}, generated={statistics.stdev(vs)}" )
     
 with open(file=fileName, mode="w") as f:
         for v in vs:
